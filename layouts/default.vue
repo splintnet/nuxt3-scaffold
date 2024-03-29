8e425ca864782dc8c3b5de7f178a708f6b08feff
Jonas Imping <j.imping@splintnet.de>
2024-03-29 18:05:56 +1030

    prettier tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <div class="flex-1 container mx-auto z-0 px-4 py-6 lg:px-0">
+  <div class="flex min-h-screen flex-col">
+    <div class="container z-0 mx-auto flex-1 px-4 py-6 lg:px-0">
       <slot />
     </div>
   </div>
