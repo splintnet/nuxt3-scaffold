b5adc154a6643b54081ab8d4496f49ba3d33e5e4
Jonas Imping <j.imping@splintnet.de>
2024-03-29 17:32:07 +1030

    updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,43 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
-  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
-  plugins: [],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    },
+    extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+    plugins: [],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 
-    'vue/max-attributes-per-line': ['error', {
-      singleline: {
-        max: 1,
-      },
-      multiline: {
-        max: 1,
-      },
-    }],
+        'vue/max-attributes-per-line': ['error', {
+            singleline: {
+                max: 1,
+            },
+            multiline: {
+                max: 1,
+            },
+        }],
 
-    'vue/no-v-html': 'off',
-    'vue/no-mutating-props': 'off',
-    'vue/one-component-per-file': 'off',
-    'vue/no-template-shadow': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/no-reserved-component-names': 'off',
-    'no-unused-vars': 'off',
-    'vue/no-multiple-template-root': 'off',
-    'vue/require-prop-types': 'off',
-    'vue/no-v-model-argument': 'off',
-  },
+        'vue/no-v-html': 'off',
+        'vue/no-mutating-props': 'off',
+        'vue/one-component-per-file': 'off',
+        'vue/no-template-shadow': 'off',
+        'vue/multi-word-component-names': 'off',
+        'vue/require-default-prop': 'off',
+        'vue/no-reserved-component-names': 'off',
+        'no-unused-vars': 'off',
+        'vue/no-multiple-template-root': 'off',
+        'vue/require-prop-types': 'off',
+        'vue/no-v-model-argument': 'off',
+    },
+    settings: {
+        "import/resolver": {
+            typescript: {} // this loads <rootdir>/tsconfig.json to eslint
+        },
+    },
 }
--- plugins/bootstrap.js
+++ plugins/bootstrap.js
@@ -6,9 +6,8 @@ import "dayjs/locale/de";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  // eslint-disable-next-line import/no-named-as-default-member
   dayjs.locale("de");
-  // eslint-disable-next-line import/no-named-as-default-member
+
   dayjs.extend(customParseFormat);
 
   nuxtApp.vueApp.use(Notifications);
